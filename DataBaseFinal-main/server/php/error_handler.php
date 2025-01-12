60c745403c0dbc1363e8ce7f758d9f1315b5eb9f
FriskOuO <C112156226@nkust.edu.tw>
2025-01-12 21:34:04 +0800

    更新admin_user_home

--- DataBaseFinal-main/server/php/error_handler.php
+++
@@ -1,9 +0,0 @@
-<?php
-session_start();
-if (isset($_SESSION['errors'])) {
-    foreach ($_SESSION['errors'] as $error) {
-        echo '<p class="error">' . htmlspecialchars($error) . '</p>';
-    }
-    unset($_SESSION['errors']);
-}
-?>
