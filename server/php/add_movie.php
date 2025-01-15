6c0b8349384a16473b9c54d963a6e3a9db08de95
FriskOuO <C112156226@nkust.edu.tw>
2025-01-15 14:58:37 +0800

    影片新增功能完成

--- server/php/add_movie.php
+++ server/php/add_movie.php
@@ -11,7 +11,7 @@ $movie_id = $_POST['movie_id'];
 $movie_type = $_POST['movie_type'];
 $movie_name = $_POST['movie_name'];
 $movie_lv = $_POST['movie_lv'];
-$member_account = $_SESSION['member_account']; // 從 session 中獲取會員帳號
+$member_account = $_POST['member_account']; // 從表單中獲取會員帳號
 
 // 錯誤檢查
 $errors = [];
@@ -36,22 +36,34 @@ if (empty($member_account)) {
     $errors[] = '會員帳號不能為空';
 }
 
-// 如果有錯誤，返回錯誤信息
-if (!empty($errors)) {
-    $_SESSION['errors'] = $errors;
-    header('Location: ../../front/html/add_movie.html');
+// 檢查資料庫中是否有相同的影片編號
+$query = $pdo->prepare("SELECT * FROM movie WHERE movie_id = ?");
+$query->execute([$movie_id]);
+if ($query->rowCount() > 0) {
+    header("Location: ../../front/html/error_movie_exists.html");
     exit();
 }
 
-// 插入數據到資料庫
-try {
-    $stmt = $pdo->prepare("INSERT INTO movies (movie_id, movie_type, movie_name, movie_lv, member_account) VALUES (?, ?, ?, ?, ?)");
-    $stmt->execute([$movie_id, $movie_type, $movie_name, $movie_lv, $member_account]);
-    $_SESSION['success'] = '影片新增成功';
-} catch (PDOException $e) {
-    $_SESSION['errors'] = ['資料庫錯誤: ' . $e->getMessage()];
+// 檢查添加者帳號是否存在且為管理者
+$query = $pdo->prepare("SELECT * FROM member WHERE member_account = ? AND member_role = 'admin'");
+$query->execute([$member_account]);
+if ($query->rowCount() == 0) {
+    header("Location: ../../front/html/error_invalid_account.html");
+    exit();
 }
 
-header('Location: ../../front/html/add_movie.html');
+// 如果有其他錯誤，將錯誤訊息存入 session 並重定向到錯誤頁面
+if (!empty($errors)) {
+    $_SESSION['errors'] = $errors;
+    header("Location: ../../front/html/error.html");
+    exit();
+}
+
+// 插入新影片資料
+$insertQuery = $pdo->prepare("INSERT INTO movie (movie_id, movie_type, movie_name, movie_lv, member_account) VALUES (?, ?, ?, ?, ?)");
+$insertQuery->execute([$movie_id, $movie_type, $movie_name, $movie_lv, $member_account]);
+
+// 新增影片成功後重定向到成功頁面
+header("Location: ../../front/html/success_add_movie.html");
 exit();
 ?>
